a3773be117a2e4333b2941431a7d639ab61e5f67
Mustafa Calik <50895577+mcalik77@users.noreply.github.com>
2021-02-27 15:56:00 -0700

    created player and movement

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -10,21 +10,42 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Space Invaders')
 
 #load the image
-playerImage = pygame.image.load('pygame/space-invaders.png')
+playerImage = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
 
+# def enemy(x,y):
+#     screen.blit(enemyImage,(x,y))
 
 def player(x,y):
     screen.blit(playerImage,(x,y))
-
+    
 
 running = True
+
 while running:
-    # Screen color
+    # Screen color RGB - Red - Green - Blue 
     screen.fill((0,0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change -= 0.3 
+            if event.key == pygame.K_RIGHT:
+                playerX_change += 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    #movement of player     
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
+
     #place the image on game windows
-    player(370,480)
+    player(playerX,playerY)
     pygame.display.update()
